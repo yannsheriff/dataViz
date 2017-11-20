a00ca1556281ac71a74a6ed47f0f3b4c274e0c0a
CHERIF Yannis <ycherif@g-a101-36.local>
2017-11-20 17:39:38 +0100

    add img

--- newDataViz/sketch.js
+++ newDataViz/sketch.js
@@ -1,6 +1,11 @@
 var quartCircles = Array()
 var img;
 
+function preload() { 
+	img = loadImage("assets/Cercle_journee.png"); 
+}
+
+
 function setup() {
 	createCanvas(windowWidth, windowHeight)
 	var opt = {
@@ -14,19 +19,21 @@ function setup() {
 	opt.rotate = 280
 
 	quartCircles.push(new QuartCircle(opt))
+
+	background('#222222');
 }
 
 function draw() {
 	clear();
 
-	// push()
-	// stroke(0,0,0)
-	// noFill()
+	push()
+	stroke(0,0,0)
+	noFill()
 
 	translate(windowWidth/2, windowHeight/2);
 	// circle = arc(0, 0, 300, 300, 0, 2 * PI );
 
-	img = createImage(300, 300);
+	// img = createImage(300, 300);
 
 
 	pop()
