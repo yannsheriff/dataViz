e996159e8c9546ad5f72f0b87168bedd80947f6e
yannsheriff <yanncherif@live.fr>
2017-11-20 23:38:48 +0100

    interaction + haldDayCircle

--- newDataViz/HalfCircle.js
+++ newDataViz/HalfCircle.js
@@ -1,15 +1,15 @@
-function HourBar(  ) { 
+function HalfCircle() { 
     
 
     this.x = windowWidth/2
     this.y = windowHeight/2
     this.pos = 1.5 
-    this.radius = 220
+    this.radius = 255
     this.hour = 1
 
 }
 
-HourBar.prototype = {
+HalfCircle.prototype = {
     update: function () {
         push()
         stroke(255,255,255, 50)
@@ -19,19 +19,18 @@ HourBar.prototype = {
         translate(this.x, this.y);
 
         rotate(this.pos * PI)
-        circle = arc(0, 0, this.radius, this.radius, 0, .222 * PI );
+        circle = arc(0, 0, this.radius, this.radius, 0, 1 * PI );
 
         pop()
     }, 
-
-    nextHour: function() {
-        TweenLite.to(this, 1, {pos: this.pos + .222});
-        this.hour++
+    nextHalf: function() {
+        TweenLite.to(this, 1, {pos: this.pos + 1});
     }, 
 
-    prevHour: function() {
-        TweenLite.to(this, 1, {pos: this.pos - .222});
-        this.hour--
+    prevHalf: function() {
+        TweenLite.to(this, 1, {pos: this.pos + 1});
     }
+
+
 }
 
--- newDataViz/sketch.js
+++ newDataViz/sketch.js
@@ -10,10 +10,10 @@ function preload() {
 function setup() {
 	createCanvas(windowWidth, windowHeight)
 	var opt = {
-		x:     windowWidth/2 , 
-		y:     windowHeight/2,
+		x: windowWidth/2, 
+		y: windowHeight/2,
 		posInit : 0,
-		radius : 350, 
+		radius : 350,
 		rotate : 0.01
 	}
 
@@ -23,6 +23,7 @@ function setup() {
 
 	quartCircles.push(new QuartCircle(opt))
 	hourBar = new HourBar()
+	halfCircle = new HalfCircle()
 	
 }
 
@@ -40,23 +41,52 @@ function draw() {
 
 	image(img, -150, -150, 300, 300);
 
-
 	pop()
-
-
 
 	quartCircles.forEach(function (quartCircle, i) {
 		quartCircle.update()
 	})
 
+
+
+	stroke(255,255,255)
+	rect(windowWidth/2 + 300, windowHeight/2 - 70, 55, 55);
+	rect(windowWidth/2 + 300, windowHeight/2 + 70, 55, 55);
+
 	hourBar.update()
-	
+	halfCircle.update()
+
 }
 
 function mousePressed() {
+
 	// Check if mouse is inside the circle
 	var d = dist(mouseX, mouseY, windowWidth/2, windowHeight/2);
 	if (d > 130 && d < 170) {
 		console.log('fuck you')
+	}
+
+	e = dist(mouseX, mouseY, windowWidth/2 + 325 , windowHeight/2 - 50);
+
+	if (e < 40 ) {
+		hourBar.prevHour()
+		if (hourBar.hour == 5 || hourBar.hour == 10 ) {
+			halfCircle.prevHalf()
+			if (hourBar.hour == 0) {
+				hourBar.hour = 10
+			}
+		}
+	}
+
+	f = dist(mouseX, mouseY, windowWidth/2 + 325, windowHeight/2 + 90);
+
+	if (f < 40) {
+		hourBar.nextHour()
+		if (hourBar.hour == 5 || hourBar.hour == 10 ) {
+			halfCircle.nextHalf()
+			if (hourBar.hour == 10) {
+				hourBar.hour = 1
+			}
+		}
 	}
   }
